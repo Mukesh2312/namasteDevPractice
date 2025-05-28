b6a295a5cb6385ac5355ff3454433d723cdf41d4
Mukesh2312 <49585682+Mukesh2312@users.noreply.github.com>
2025-05-28 18:10:40 +0530

    creating first route

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ const app = express();
 app.use("/", (req, res) => {
     res.send("Hello there...")
 })
+app.use("/first", (req, res) => {
+    res.send("this is first route")
+})
 app.listen(7777, () => {
     console.log("server is running on 7777");
 })
