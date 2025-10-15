046ba34df138a53bdd0c2eef3c6a6b9296b8cc89
Mukesh2312 <49585682+Mukesh2312@users.noreply.github.com>
2025-10-15 19:21:10 +0530

    view all connection

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,8 +4,10 @@ const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const { ConnectionRequest } = require("../models/connectionRequest")
 
+
+let userSafeData = "firstName lastName skills age photoUrl";
 // get all the received connection request
-userRouter.get("/user/connection/received", userAuth, async (req, res) => {
+userRouter.get("/user/connections/received", userAuth, async (req, res) => {
     try {
         const loggedInUser = req.user;
 
@@ -17,6 +19,37 @@ userRouter.get("/user/connection/received", userAuth, async (req, res) => {
         res.json({ message: "Data received", data: connectionRequestReceived })
     } catch (error) {
         res.status(400).json({ message: "Something went wrong", error: error.message })
+    }
+});
+
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+
+        const connectionsList = await ConnectionRequest.find({
+            $or: [
+                { toUserId: loggedInUser._id, status: "accepted" },
+                { fromUserId: loggedInUser._id, status: "accepted" },
+            ]
+        })
+            .populate("fromUserId", userSafeData)
+            .populate("toUserId", userSafeData)
+
+        console.log(connectionsList)
+        const data = connectionsList.map((row) => {
+
+            if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+                return row.toUserId;
+            } else {
+                return row.fromUserId;
+            }
+        });
+
+        res.json({ message: "data received successfully", data })
+    } catch (error) {
+        res.status(400).json({ message: "Something went wrong", error: error.message })
+
     }
 })
 module.exports = userRouter;
